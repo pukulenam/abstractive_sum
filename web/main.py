74549fbcee9938e619836ca5be6d4af75ea24503
tolobret <achmad.nofandi@gmail.com>
2022-05-13 20:23:02 +0700

    test deploy AI Platform

--- web/main.py
+++ web/main.py
@@ -23,17 +23,36 @@ def home():
     if request.method == 'POST':
         news_text = request.form['inputNews']
 
-        summarized='Intinya... '+str(news_text)
+        # summarized='Intinya... '+str(news_text)
         
-        new_news = News(inputNews=news_text,summarizedNews=summarized)
+        # new_news = News(inputNews=news_text,summarizedNews=summarized)
 
         try:
             # return news_text
+            # db.session.add(new_news)
+            # db.session.commit()
+            
+            
+            # return render_template('index.html', news_text=news_text,summarized=summarized)
+            
+
+            # Test AI Platform 
+
+            
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "pukulenam-test-24bfaa30d863.json" # change for your GCP key
+            PROJECT = "pukulenam-test" # change for your GCP project
+            REGION = "us-central1" # change for your GCP region (where your model is hosted)
+            MODEL = 'test'
+
+            payload=[news_text]
+            output = predict_json(PROJECT,REGION,MODEL,payload)
+            str_output = ' '.join(map(str, output))
+            new_news = News(inputNews=news_text,summarizedNews=str_output)
             db.session.add(new_news)
             db.session.commit()
-            
-            # return redirect('/')
-            return render_template('index.html', news_text=news_text,summarized=summarized)
+            return render_template('index.html',news_text=news_text,summarized=str_output)
+
+
         except:
             return 'There was an issue summarizing your news'
 
@@ -67,6 +86,51 @@ def home_(id):
 
     return render_template('index.html', news=result)
     # return send_from_directory(basedir,'exported.csv')
+
+import googleapiclient.discovery
+import json
+from google.api_core.client_options import ClientOptions
+
+service = googleapiclient.discovery.build('ml', 'v1')
+
+def predict_json(project, region, model, instances, version=None):
+    """Send json data to a deployed model for prediction.
+
+    Args:
+        project (str): project where the Cloud ML Engine Model is deployed.
+        region (str): regional endpoint to use; set to None for ml.googleapis.com
+        model (str): model name.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of datatypes
+            convertible to tensors.
+        version: str, version of the model to target.
+    Returns:
+        Mapping[str: any]: dictionary of prediction results defined by the
+            model.
+    """
+    # Create the ML Engine service object.
+    # To authenticate set the environment variable
+    # GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
+    prefix = "{}-ml".format(region) if region else "ml"
+    api_endpoint = "https://{}.googleapis.com".format(prefix)
+    client_options = ClientOptions(api_endpoint=api_endpoint)
+    service = googleapiclient.discovery.build(
+        'ml', 'v1', client_options=client_options)
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances}
+    ).execute()
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
 
 
 if __name__ == "__main__":
